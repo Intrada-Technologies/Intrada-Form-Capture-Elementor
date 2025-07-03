75a822036827c8207e09b6540f3695485ed13ef1
Adam Post <adamrpostjr@gmail.com>
2025-07-03 14:59:56 -0400

    Maybe fixing some update issues

--- includes/update.php
+++ includes/update.php
@@ -5,6 +5,47 @@ class intrada_form_capture_plugin_update
   public function __construct()
   {
     add_filter('plugins_api', array($this, 'plugin_info'), 20, 3);
+    add_filter('site_transient_update_plugins', array($this, 'push_update'));
+  }
+
+  public function push_update($transient)
+  {
+    if (empty($transient->checked)) {
+      return $transient;
+    }
+
+    $remote = wp_remote_get('https://raw.githubusercontent.com/Intrada-Technologies/Intrada-Form-Capture-Elementor/main/info.json', array(
+      'timeout' => 10,
+      'headers' => array('Accept' => 'application/json')
+    ));
+
+    if (
+      is_wp_error($remote)
+      || 200 !== wp_remote_retrieve_response_code($remote)
+      || empty(wp_remote_retrieve_body($remote))
+    ) {
+      return $transient;
+    }
+
+    $remote = json_decode(wp_remote_retrieve_body($remote));
+
+    // Path to your main plugin file
+    $plugin = 'intrada-elementor-form-capture/intrada-elementor-form-capture.php';
+
+    if (
+      version_compare($remote->version, $transient->checked[$plugin], '>')
+    ) {
+      $obj = new stdClass();
+      $obj->slug = $remote->slug;
+      $obj->plugin = $plugin;
+      $obj->new_version = $remote->version;
+      $obj->url = $remote->homepage ?? '';
+      $obj->package = $remote->download_url;
+
+      $transient->response[$plugin] = $obj;
+    }
+
+    return $transient;
   }
 
   public function plugin_info($res, $action, $args)
@@ -17,7 +58,7 @@ class intrada_form_capture_plugin_update
     }
 
 
-    $remote = wp_remote_get('https://raw.githubusercontent.com/Intrada-Technologies/Intrada-Form-Capture-Elementor/refs/heads/main/info.json', array(
+    $remote = wp_remote_get('https://raw.githubusercontent.com/Intrada-Technologies/Intrada-Form-Capture-Elementor/main/info.json', array(
       'timeout' => 10,
       'headers' => array(
         'Accept' => 'application/json'
